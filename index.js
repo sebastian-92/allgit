279bd87e3a93e07d1a4e19925c5ddce71b18d2e4
Cbass92 <140918855+sebastian-92@users.noreply.github.com>
2025-02-12 14:44:09 -0500

    oopsie poopsie

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 const fetch = require('node-fetch');
-var mime = require('mime")
+var mime = require('mime')
 var port = process.env.PORT || 3000;
 app.use(express.static("public"));
 
